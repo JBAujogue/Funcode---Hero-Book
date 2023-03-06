71e16e1eec263f44be7959b73ffd71bcfb1fcf78
Jean-baptiste Aujogue <jb.aujogue@gmail.com>
2023-03-06 08:29:58 +0100

    Update app.bat

--- streamlit/app.bat
+++ streamlit/app.bat
@@ -1,6 +1,5 @@
 @ECHO OFF
-call C:\Users\jbaujogue\Anaconda3\Scripts\activate.
-call C:/ProgramData/Anaconda3/Scripts/activate.bat
+call C:/Users/jb/miniconda3/Scripts/activate.bat
 cd %~dp0
 call conda activate herobook
 streamlit run app.py --server.port 8340
